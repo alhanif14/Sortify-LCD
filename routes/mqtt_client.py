4b81249f8139703a4a4d80758e379ff4b3936d7c
alhanif14 <azizhanif1405@gmail.com>
2025-07-29 21:11:07 +0700

    change mqtt logic

--- routes/mqtt_client.py
+++ routes/mqtt_client.py
@@ -1,40 +1,40 @@
-import threading
-import paho.mqtt.client as mqtt
+# import threading
+# import paho.mqtt.client as mqtt
 
-MQTT_BROKER = "broker.emqx.io"
-MQTT_PORT = 1883
-MQTT_KEEPALIVE = 60
+# MQTT_BROKER = "broker.emqx.io"
+# MQTT_PORT = 1883
+# MQTT_KEEPALIVE = 60
 
-_last_mqtt_result = None
-_lock = threading.Lock()
+# _last_mqtt_result = None
+# _lock = threading.Lock()
 
-client = mqtt.Client()
+# client = mqtt.Client()
 
-def on_connect(client, userdata, flags, rc):
-    print("Connected to MQTT broker with result code " + str(rc))
-    client.subscribe("waste/raw")
+# def on_connect(client, userdata, flags, rc):
+#     print("Connected to MQTT broker with result code " + str(rc))
+#     client.subscribe("waste/raw")
 
-def on_message(client, userdata, msg):
-    global _last_mqtt_result
-    payload = msg.payload.decode('utf-8')
-    print(f"MQTT received message: {payload}")
-    with _lock:
-        _last_mqtt_result = payload
+# def on_message(client, userdata, msg):
+#     global _last_mqtt_result
+#     payload = msg.payload.decode('utf-8')
+#     print(f"MQTT received message: {payload}")
+#     with _lock:
+#         _last_mqtt_result = payload
 
-client.on_connect = on_connect
-client.on_message = on_message
+# client.on_connect = on_connect
+# client.on_message = on_message
 
-client.connect(MQTT_BROKER, MQTT_PORT, MQTT_KEEPALIVE)
-client.loop_start()
+# client.connect(MQTT_BROKER, MQTT_PORT, MQTT_KEEPALIVE)
+# client.loop_start()
 
-def mqtt_publish(topic: str, message: str):
-    client.publish(topic, message)
+# def mqtt_publish(topic: str, message: str):
+#     client.publish(topic, message)
 
-def get_last_mqtt_result():
-    with _lock:
-        return _last_mqtt_result
+# def get_last_mqtt_result():
+#     with _lock:
+#         return _last_mqtt_result
 
-def reset_last_mqtt_result():
-    global _last_mqtt_result
-    with _lock:
-        _last_mqtt_result = None
+# def reset_last_mqtt_result():
+#     global _last_mqtt_result
+#     with _lock:
+#         _last_mqtt_result = None
